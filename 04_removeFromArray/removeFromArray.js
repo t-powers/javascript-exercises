1f774333fe62893b4cfe145575a192a508fa4bd5
t-powers <127955989+t-powers@users.noreply.github.com>
2024-06-25 21:17:45 -0400

    Finished problem

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,6 @@
-const removeFromArray = function () {
-    
-  
-};
+function removeFromArray(array, ...itemsToRemove) {
+  return array.filter((item) => !itemsToRemove.includes(item));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
