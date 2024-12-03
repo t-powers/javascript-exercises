7616bdd5205e30f7de468704ad54dddf1649f782
t-powers <127955989+t-powers@users.noreply.github.com>
2024-12-02 22:06:20 -0500

    Added non-int logic

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,12 @@
 const sumAll = function (a, b) {
-    let sum = 0;
-    for (let i = a; i <= b; i++){
-        sum += i
-        
+  let sum = 0;
+  for (let i = a; i <= b; i++) {
+    if (a < 0 || b < 0) return "ERROR";
+    else {
+      sum += i;
     }
-    return sum
+  }
+  return sum;
 };
 
 module.exports = sumAll;
