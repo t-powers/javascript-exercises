f2683a7eca6ab2ffb0d0f60134c321a06d8e3681
t-powers <127955989+t-powers@users.noreply.github.com>
2024-12-03 20:42:29 -0500

    Return error if parament isNaN

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,8 +1,11 @@
 const sumAll = function (a, b) {
   let sum = 0;
   for (let i = a; i <= b; i++) {
-    if (a < 0 || b < 0) return "ERROR";
-    else {
+    if (a < 0 || b < 0) {
+      return "ERROR";
+    } else if (isNaN(a) === false || isNaN(b) === false) {
+      return "ERROR";
+    } else {
       sum += i;
     }
   }
