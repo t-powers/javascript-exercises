7c4d0760e0acd377b40345a3be6c1426b1e7ca43
t-powers <127955989+t-powers@users.noreply.github.com>
2024-12-03 21:32:21 -0500

    Reset for do-over

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,15 +1,10 @@
 const sumAll = function (a, b) {
-  let sum = 0;
-  for (let i = a; i <= b; i++) {
-    if (a < 0 || b < 0) {
-      return "ERROR";
-    } else if (isNaN(a) === false || isNaN(b) === false) {
-      return "ERROR";
-    } else {
-      sum += i;
+    let sum = 0;
+    for (let i = a; i <= b; i++){
+        sum += i
+        
     }
-  }
-  return sum;
+    return sum
 };
 
 module.exports = sumAll;
