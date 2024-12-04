c9c7cb3665a7f7c766d5875653a7435a94473027
t-powers <127955989+t-powers@users.noreply.github.com>
2024-12-04 18:51:49 -0500

    Excluding non numbers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,15 @@
 const sumAll = function (a, b) {
-    let sum = 0;
-    for (let i = a; i <= b; i++){
-        sum += i
-        
-    }
-    return sum
+  let sum = 0;
+
+  if (a < 0 || b < 0) {
+    return "ERROR";
+  } else if (typeof a !== "number" || typeof b !== "number") {
+    return "ERROR";
+  }
+  for (let i = a; i <= b; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 module.exports = sumAll;
