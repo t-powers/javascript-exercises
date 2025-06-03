86c2f590ee49e16c652b029830643d67cbeb9f24
t-powers <127955989+t-powers@users.noreply.github.com>
2025-06-03 14:59:33 -0400

    Config'd my settings.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,39 +1,40 @@
 const add = function (a, b) {
-  return a + b;
+    return a + b;
 };
 
 const subtract = function (a, b) {
-  return a - b;
+    return a - b;
 };
 
 const sum = function (arr) {
-  return arr.reduce(
-    (accumulator, currentValue) => accumulator + currentValue,
-    0
-  );
+    return arr.reduce(
+        (accumulator, currentValue) => accumulator + currentValue,
+        0
+    );
 };
 
 const multiply = function (arr) {
-  return arr.reduce((acc, currentValue) => acc * currentValue, 1);
+    return arr.reduce((acc, currentValue) => acc * currentValue, 1);
 };
 
 const power = function (a, b) {
-  return a ** b;
+    return a ** b;
 };
 
 const factorial = function (n) {
-  if (n === 0 || n === 1) {
-    return 1;
-  }
-  return n * factorial(n - 1);
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
 };
 
+//just a test
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial,
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
