21bbb8ac5b1a25afef8e3c77cea9a9df6a67ad5b
t-powers <127955989+t-powers@users.noreply.github.com>
2025-06-04 14:48:03 -0400

    Update solution, start findTheOldest.js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+//Your job is to write a function that takes the array and returns an array of titles:
+//function getTheTitles()
+//loop through array
+//obtain first position of each which is title
+//push title into empty array
 
+const getTheTitles = function (array) {
+    return array.map((book) => book.title)
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function () {
 
 };
 
+//
+// const people = [
+//     {
+//         name: "Carly",
+//         yearOfBirth: 1942,
+//         yearOfDeath: 1970,
+//     },
+//     {
+//         name: "Ray",
+//         yearOfBirth: 1962,
+//         yearOfDeath: 2011,
+//     },
+//     {
+//         name: "Jane",
+//         yearOfBirth: 1912,
+//         yearOfDeath: 1941,
+//     },
+// ]
+// Given an array of objects representing people with a birth and death year, return the oldest person.
+//
+// Now that you've reached the final exercise, you should be fairly comfortable getting the information
+// you need from test case(s). Take a look at how the array of objects is constructed in this exercise's
+// test cases to help you write your function.
+//
 // Do not edit below this line
 module.exports = findTheOldest;
